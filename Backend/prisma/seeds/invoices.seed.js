d047c8f46dd3ad3c9e23af97f9b742548cf17b77
Pham Huynh Long <phamhuynhlong20@gmail.com>
2022-03-05 16:45:30 +0700

    seed-invoices

---
+++ Backend/prisma/seeds/invoices.seed.js
@@ -0,0 +1,246 @@
+module.exports.invoices = [{
+
+        id: 'TEST1',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Đỗ Phương Thảo',
+        description: 'Thu tiền học tháng 08 niên học 20-21',
+        ammount: 10000,
+        tel1: '0913492818',
+        month: '08',
+        BHYT: 'AGKT@'
+
+    },
+    {
+
+        id: 'TEST2',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Đỗ Phương Thảo',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0913492818',
+        month: '09',
+        BHYT: 'AGKT@'
+
+    },
+    {
+
+        id: 'TEST3',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Đỗ Phương Thảo',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0913492818',
+        month: '09',
+        BHYT: 'AGKT@'
+
+    },
+    {
+
+        id: 'TEST4',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Đỗ Phương Thảo',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0913492818',
+        month: '09',
+        BHYT: 'AGKT@'
+
+    },
+    {
+
+        id: 'TEST5',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Đỗ Phương Thảo',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0913492818',
+        month: '09',
+        BHYT: 'AGKT@'
+
+    },
+    {
+
+        id: 'TEST6',
+        MST: '0402050829',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường VIN',
+        name: 'NGUYỄN A',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0123456789',
+        month: '09',
+        BHYT: 'BHYTFAKE'
+
+    },
+    {
+
+        id: 'TEST7',
+        MST: '0402050829',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường VIN',
+        name: 'NGUYỄN A',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0123456789',
+        month: '09',
+        BHYT: 'BHYTFAKE'
+
+    },
+    {
+
+        id: 'TEST8',
+        MST: '0402050829',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường VIN',
+        name: 'NGUYỄN A',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0123456789',
+        month: '09',
+        BHYT: 'BHYTFAKE'
+
+    },
+    {
+
+        id: 'TEST9',
+        MST: '0402050829',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường VIN',
+        name: 'NGUYỄN A',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0123456789',
+        month: '09',
+        BHYT: 'BHYTFAKE'
+
+    },
+    {
+
+        id: 'TEST10',
+        MST: '0402050829',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường VIN',
+        name: 'NGUYỄN A',
+        description: 'Thu tiền học tháng 09 niên học 20-21',
+        ammount: 10000,
+        tel1: '0123456789',
+        month: '09',
+        BHYT: 'BHYTFAKE'
+
+    },
+    {
+
+        id: 'DF06TNRWW108',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Lê Như Quỳnh',
+        description: 'Thu tiền học tháng 08 niên học 20-21',
+        ammount: 15000,
+        tel1: '0555555555',
+        month: '08',
+        BHYT: 'TNRWW'
+    },
+    {
+
+        id: 'DF06KBI@M108',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Nguyễn Lê Khã Hân',
+        description: 'Thu tiền học tháng 08 niên học 20-21',
+        ammount: 16000,
+        tel1: '0222222222',
+        month: '08',
+        BHYT: 'KBI@M'
+
+    },
+    {
+
+        id: 'DF06TNINB108',
+        MST: '0400912226',
+        classCode: '01.TRE2',
+        className: 'Nhà trẻ 2',
+        schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+        name: 'Lê Phạm Tịnh Nhi',
+        description: 'Thu tiền học tháng 08 niên học 20-21',
+        ammount: 1555000,
+        tel1: '0111111111',
+        month: '08',
+        BHYT: 'TNINB'
+    },
+
+]
+
+// {
+
+//     id: 'DF06HIHTQ108',
+//     MST: '0400912226',
+//     classCode: '01.TRE2',
+//     className: 'Nhà trẻ 2',
+//     schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+//     name: 'Nguyễn Hà Thiên Minh',
+//     description: 'Thu tiền học tháng 08 niên học 20-21',
+//     ammount: 1060000,
+//     tel1: '0913492818',
+//     month: '08',
+//     updateDate: '20210826',
+//     bank: 'STB',
+//     BHYT: 'HIHTQ'
+
+// },
+
+
+
+// {
+
+//     id: 'DF06AGKT@108',
+//     MST: '0400912226',
+//     classCode: '01.TRE2',
+//     className: 'Nhà trẻ 2',
+//     schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+//     name: 'Đỗ Phương Thảo',
+//     description: 'Thu tiền học tháng 08 niên học 20-21',
+//     ammount: 1555000,
+//     tel1: '0913492818',
+//     month: '08',
+//     BHYT: 'AGKT@'
+
+// },
+// {
+
+//     id: 'DF06AGKYA108',
+//     MST: '0400912226',
+//     classCode: '01.TRE2',
+//     className: 'Nhà trẻ 2',
+//     schoolName: 'Trường Mầm non Bạch Yến - Sơn Trà',
+//     name: 'Phan Phương Anh',
+//     description: 'Thu tiền học tháng 08 niên học 20-21',
+//     ammount: 1300000,
+//     tel1: '0913492818',
+//     month: '08',
+//     BHYT: 'AGKYA'
+
+// },
