8a8e2ac0a7dac27ad0fe1629aba9264ce5959894
dnpnghia <idnpnlol@gmail.com>
2022-03-05 17:04:57 +0700

    controller-bill_module get all bill by token

--- Backend/controllers/bill.controller.js
+++ Backend/controllers/bill.controller.js
@@ -31,3 +31,47 @@ module.exports.findAll = async function(req, res) {
         await prisma.$disconnect()
     }
 }
+
+module.exports.findAllByToken = async function(req, res) {
+  try {
+      if (req.user.accRole === 'PARENT') {
+          const parent = await prisma.parent.findUnique({
+              where: { accountId: req.user.id }
+          });
+          var allBill = await prisma.bill.findMany({
+              where: { parentId: parent.id },
+              include: {
+                  bill_mapping_history: true,
+              }
+          });
+          if (allBill.length === 0) {
+              res.status(400).json({ ok: false, message: "Không tìm thấy bill nào!" });
+          }
+          res.json({ ok: true, message: "Get all bills successfully!", data: allBill });
+      }
+      if (req.user.accRole === 'SCHOOL') {
+          const school = await prisma.school.findUnique({
+              where: { accountId: req.user.id }
+          });
+          const allBill = await prisma.bill.findMany({
+              where: { MST: school.MST },
+              include: {
+                  bill_mapping_history: true,
+              }
+          });
+          if (allBill.length === 0) {
+              res.status(400).json({ ok: false, message: "Không tìm thấy bill nào!" });
+          }
+          res.json({ ok: true, message: "Get all bills successfully!", data: allBill });
+      }
+      res.status(400).json({ ok: false, message: "Không tìm thấy bill nào!" });
+  } catch (error) {
+      res.status(500).json({
+          ok: false,
+          message: "Somthing went wrong"
+      });
+  } finally {
+      async() =>
+      await prisma.$disconnect()
+  }
+}
