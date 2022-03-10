09fe3b5ee3b501daabb5370e32497d26e95ecb78
khanhln <khanhln1570@gmail.com>
2022-03-10 22:35:46 +0700

    utils.helper.js

---
+++ Frontend/helpers/errorHandle.helper.js
@@ -0,0 +1,18 @@
+const errorHandle = (err, duration = 3000) => {
+    if (process.server) {
+      return
+    }
+    if (!$nuxt) return;
+  
+    let errorMessage = 'Something went wrong!';
+    if (err.response && err.response.data) {
+      errorMessage = err.response.data?.message;
+    }
+    console.log("err", err.response);
+  
+    return $nuxt?.$toast?.error(errorMessage, {
+      duration: duration
+    });
+  }
+  
+  export default errorHandle;
