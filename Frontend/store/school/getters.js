3eadbe7ad188dc6033512d693bc65052ae062444
Nguyễn Huỳnh Thanh Lâm <>
2022-03-10 23:00:27 +0700

    add payment, school store

---
+++ Frontend/store/school/getters.js
@@ -0,0 +1,8 @@
+export default {
+  getSchools(state) {
+    return state.schools;
+  },
+  getCountSchool(state) {
+    return state.countSchool;
+  },
+};
---
+++ Frontend/store/school/state.js
@@ -0,0 +1,4 @@
+export default () => ({
+  schools: [],
+  countSchool: 0,
+});
