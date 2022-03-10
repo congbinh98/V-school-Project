9daed988eb4e3bff8e0702c67dfe39d5c29dc9a0
Nguyễn Huỳnh Thanh Lâm <>
2022-03-10 22:58:03 +0700

    add auth, bill, detailsSchool store

---
+++ Frontend/store/invoice/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  invoiceTypes: [
+    // {
+    //   id: "EXTRA_TUITION",
+    //   label: "Phụ đạo"
+    // },
+    {
+      id: "TUITION",
+      label: "Học phí"
+    },
+    // {
+    //   id: "FUNDS",
+    //   label: "Quỹ lớp"
+    // },
+    // {
+    //   id: "LEARNING_TOOL",
+    //   label: "Dụng cụ học tập"
+    // },
+  ],
+  countInvoice: 0,
+  invoices: [],
+  currentInvoice: null,
+});
